dc224d5e2f3f2e462c2d5db056a5042c5d922905
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-04-12 17:22:42 -0700

    starter code copied

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
-// connect to mongoose when starting the app
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza/pizza-hunt', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-// Use this to log mongo queries being executed
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
