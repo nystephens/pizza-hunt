4207066c5df1cd37bdc2eda14700fb82247053a5
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-04-13 09:59:34 -0700

    end 18.3 before starter code

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const $backBtn = document.querySelector('#back-btn');
 const $pizzaName = document.querySelector('#pizza-name');
 const $createdBy = document.querySelector('#created-by');
@@ -106,6 +108,28 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
+  fetch(`/api/comments/${pizzaId}/${commentId}` , {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Conent-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  })
+  .then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 
 $backBtn.addEventListener('click', function() {
